aaeed8a808933b8e8894fd5029d78bd0bc412dc3
Sahil <sahilparekh1212@gmail.com>
2022-10-12 00:04:52 -0400

    1)Code Formatting

--- JS/main.js
+++ JS/main.js
@@ -23,25 +23,25 @@ function updateHeader() {
     const header = document.querySelector('header');
     header.innerHTML = `
         <nav class="navbar navbar-expand-lg">
-        <div class="container">
-            <a href="#" class="navbar-brand">javaScript Project</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menuItems">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="menuItems">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">A</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">B</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">C</a>
-                    </li>
-                </ul>
+            <div class="container">
+                <a href="#" class="navbar-brand">javaScript Project</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menuItems">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="menuItems">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">A</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">B</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">C</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
         </nav>
     `;
 }
@@ -55,31 +55,31 @@ function updateMain() {
 function updateFooter() {
     const footer= document.querySelector('footer');
     footer.innerHTML=`
-    <div class="container">
-        <div class="row d-flex">
-            <div class="col-md-2 my-auto">
-                <span class="my-auto p-3">Follow Us:</span>
+        <div class="container">
+            <div class="row d-flex">
+                <div class="col-md-2 my-auto">
+                    <span class="my-auto p-3">Follow Us:</span>
+                </div>
+                <div class="col-md-3 p-2 my-auto justify-content-around">        
+                    <a href="https://www.youtube.com/" target="_blank"><i class="bi-youtube p-3" aria-hidden="true"></i></a>
+                    <a href="https://twitter.com/?lang=en" target="_blank"><i class="bi-twitter p-3" aria-hidden="true"></i></a>
+                    <a href="https://www.instagram.com/" target="_blank"><i class="bi-instagram p-3" aria-hidden="true"></i></a>
+                </div>
             </div>
-            <div class="col-md-3 p-2 my-auto justify-content-around">        
-                <a href="https://www.youtube.com/" target="_blank"><i class="bi-youtube p-3" aria-hidden="true"></i></a>
-                <a href="https://twitter.com/?lang=en" target="_blank"><i class="bi-twitter p-3" aria-hidden="true"></i></a>
-                <a href="https://www.instagram.com/" target="_blank"><i class="bi-instagram p-3" aria-hidden="true"></i></a>
+            <div class="row d-flex">
+                <div class="col-md-2 my-auto">
+                    <span class="my-auto p-3">Logo</span>
+                </div>
+                <div class="col-md-5 p-2 my-auto justify-content-around">        
+                    <a href="#" target="_blank" class="text-decoration-none"><span class="p-3">Privacy and Terms</span></a>
+                    <a href="#" target="_blank" class="text-decoration-none"><span class="p-3">About Us</span></a>
+                    <a href="#" target="_blank" class="text-decoration-none"><span class="p-3">Our Products</span></a>
+                </div>
+                <div class="col-md-5 p-2">        
+                    <a href="#" target="_blank" class="text-decoration-none"><i class="bi bi-question p-2" aria-hidden="true"></i><span>Help</span></a>
+                </div>
             </div>
         </div>
-        <div class="row d-flex">
-            <div class="col-md-2 my-auto">
-                <span class="my-auto p-3">Logo</span>
-            </div>
-            <div class="col-md-5 p-2 my-auto justify-content-around">        
-                <a href="#" target="_blank" class="text-decoration-none"><span class="p-3">Privacy and Terms</span></a>
-                <a href="#" target="_blank" class="text-decoration-none"><span class="p-3">About Us</span></a>
-                <a href="#" target="_blank" class="text-decoration-none"><span class="p-3">Our Products</span></a>
-            </div>
-            <div class="col-md-5 p-2">        
-                <a href="#" target="_blank" class="text-decoration-none"><i class="bi bi-question p-2" aria-hidden="true"></i><span>Help</span></a>
-            </div>
-        </div>
-    </div>
     `;
 }
 
@@ -87,33 +87,33 @@ function createAccordianItem(totalDiv){
     let accordianHTML=``;
     for(i=0;i<totalDiv;i++){
         accordianHTML+=`
-        <div class="accordion-item bg-light text-dark ">
-            <h2 class="accordion-header" id="accordionHeading${i}">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#accordionCollapse${i}" aria-expanded="false"
-                    aria-controls="accordionCollapse${i}">
-                    Dashboard ${i+1}
-                </button>
-            </h2>
-        </div>
+            <div class="accordion-item bg-light text-dark ">
+                <h2 class="accordion-header" id="accordionHeading${i}">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#accordionCollapse${i}" aria-expanded="false"
+                        aria-controls="accordionCollapse${i}">
+                        Dashboard ${i+1}
+                    </button>
+                </h2>
+            </div>
         `;    
         if(i===0){
             accordianHTML+=`
-            <div id="accordionCollapse${i}" class="accordion-collapse collapse border border-dark rounded"
-                aria-labelledby="accordionHeading${i}">
-                <div class="accordion-body">
-                    ${mouseEventsLogger()}
+                <div id="accordionCollapse${i}" class="accordion-collapse collapse border border-dark rounded"
+                    aria-labelledby="accordionHeading${i}">
+                    <div class="accordion-body">
+                        ${mouseEventsLogger()}
+                    </div>
                 </div>
-            </div>
             `;
         }else{
             accordianHTML+=`
-            <div id="accordionCollapse${i}" class="accordion-collapse collapse border border-dark rounded"
-                aria-labelledby="accordionHeading${i}">
-                <div class="accordion-body">
-                    TBD
+                <div id="accordionCollapse${i}" class="accordion-collapse collapse border border-dark rounded"
+                    aria-labelledby="accordionHeading${i}">
+                    <div class="accordion-body">
+                        TBD
+                    </div>
                 </div>
-            </div>
             `;
         }
     } 
@@ -157,14 +157,14 @@ function mouseEventsLogger(){
 }
 
 function addDOMEvents(){
+
+    let mousePointerPositionTrackerTextElement=document.getElementById('mousePointerPositionTrackerText');
     window.addEventListener('mousemove',(event)=>{
-        let mousePointerPositionTrackerTextElement=document.getElementById('mousePointerPositionTrackerText');
         mousePointerPositionTrackerTextElement.innerHTML=`
             <span class="p-2">pageX= ${event.pageX}</span>
             <span class="p-2">pageY= ${event.pageY}</span>
         `;
     });
-
 
     let hoverToggle=document.getElementById('hoverToggle');
     hoverToggle.addEventListener('mouseenter',()=>{
@@ -178,7 +178,6 @@ function addDOMEvents(){
         hoverToggle.classList.toggle('text-dark');
     });
 
-
     let windowClickCounterElement = document.getElementById('windowClickCounterText');
     window.addEventListener('click',()=>{
         windowClickCount++;
@@ -190,4 +189,5 @@ function addDOMEvents(){
         windowDblClickCount++;
         windowDblClickCounterElement.innerText=`${windowDblClickCount}`;
     });
+
 }
